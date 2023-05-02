e07e6284e68899daf059e2f81cc4048e7e8907f3
FedeAle1525 <fedeale.1525@gmail.com>
2023-05-02 14:56:46 +0200

    PHP create Route and Controller 'Index' for API

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    // Metodo che servira' all'API per recuperare tutti i Progetti dal DB
+    public function index()
+    {
+
+        $results = Project::with('type', 'technologies')->orderBy('name')->get();
+
+        return response()->json([
+
+            'success' => true,
+            'results' => $results
+        ]);
+    }
+}
